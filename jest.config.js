c505a2a958c6adddb35aed639d4f0adb4ea36738
HIPgirl95 <hannah@dawne.com>
2025-08-15 11:15:30 -0400

    Making jest work with .module.css files

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    // Mock CSS modules
+    "\\.module\\.css$": "identity-obj-proxy",
+    // Mock other style files (optional)
+    "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
+  },
 };
