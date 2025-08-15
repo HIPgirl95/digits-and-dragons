dbe55462377f9b5dbe2d8dfb4972b35aa78bf4f1
HIPgirl95 <hannah@dawne.com>
2025-08-15 13:00:35 -0400

    adding questions (may not be accurate)

--- app/data/math/addition.js
+++ app/data/math/addition.js
@@ -1,12 +1,502 @@
 export const additionQuestions = [
   {
     id: 1,
-    question: "What is 4 + 4?",
+    question: "What is 1 + 2?",
     options: [
-      { text: "8", isCorrect: true },
+      { text: "3", isCorrect: true },
+      { text: "2", isCorrect: false },
       { text: "4", isCorrect: false },
       { text: "12", isCorrect: false },
-      { text: "44", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "What is 3 + 5?",
+    options: [
+      { text: "8", isCorrect: true },
+      { text: "7", isCorrect: false },
+      { text: "9", isCorrect: false },
+      { text: "35", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "What is 6 + 4?",
+    options: [
+      { text: "10", isCorrect: true },
+      { text: "9", isCorrect: false },
+      { text: "11", isCorrect: false },
+      { text: "64", isCorrect: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "What is 7 + 8?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "78", isCorrect: false },
+    ],
+  },
+  {
+    id: 5,
+    question: "What is 2 + 9?",
+    options: [
+      { text: "11", isCorrect: true },
+      { text: "10", isCorrect: false },
+      { text: "12", isCorrect: false },
+      { text: "29", isCorrect: false },
+    ],
+  },
+  {
+    id: 6,
+    question: "What is 5 + 7?",
+    options: [
+      { text: "12", isCorrect: true },
+      { text: "11", isCorrect: false },
+      { text: "13", isCorrect: false },
+      { text: "57", isCorrect: false },
+    ],
+  },
+  {
+    id: 7,
+    question: "What is 3 + 9?",
+    options: [
+      { text: "12", isCorrect: true },
+      { text: "11", isCorrect: false },
+      { text: "13", isCorrect: false },
+      { text: "39", isCorrect: false },
+    ],
+  },
+  {
+    id: 8,
+    question: "What is 8 + 6?",
+    options: [
+      { text: "14", isCorrect: true },
+      { text: "13", isCorrect: false },
+      { text: "15", isCorrect: false },
+      { text: "86", isCorrect: false },
+    ],
+  },
+  {
+    id: 9,
+    question: "What is 4 + 11?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "411", isCorrect: false },
+    ],
+  },
+  {
+    id: 10,
+    question: "What is 10 + 3?",
+    options: [
+      { text: "13", isCorrect: true },
+      { text: "12", isCorrect: false },
+      { text: "14", isCorrect: false },
+      { text: "103", isCorrect: false },
+    ],
+  },
+  {
+    id: 11,
+    question: "What is 2 + 6?",
+    options: [
+      { text: "8", isCorrect: true },
+      { text: "7", isCorrect: false },
+      { text: "9", isCorrect: false },
+      { text: "26", isCorrect: false },
+    ],
+  },
+  {
+    id: 12,
+    question: "What is 5 + 5?",
+    options: [
+      { text: "10", isCorrect: true },
+      { text: "11", isCorrect: false },
+      { text: "12", isCorrect: false },
+      { text: "55", isCorrect: false },
+    ],
+  },
+  {
+    id: 13,
+    question: "What is 3 + 7?",
+    options: [
+      { text: "10", isCorrect: true },
+      { text: "9", isCorrect: false },
+      { text: "11", isCorrect: false },
+      { text: "37", isCorrect: false },
+    ],
+  },
+  {
+    id: 14,
+    question: "What is 9 + 1?",
+    options: [
+      { text: "10", isCorrect: true },
+      { text: "9", isCorrect: false },
+      { text: "11", isCorrect: false },
+      { text: "91", isCorrect: false },
+    ],
+  },
+  {
+    id: 15,
+    question: "What is 4 + 6?",
+    options: [
+      { text: "10", isCorrect: true },
+      { text: "9", isCorrect: false },
+      { text: "11", isCorrect: false },
+      { text: "46", isCorrect: false },
+    ],
+  },
+  {
+    id: 16,
+    question: "What is 7 + 3?",
+    options: [
+      { text: "10", isCorrect: true },
+      { text: "9", isCorrect: false },
+      { text: "11", isCorrect: false },
+      { text: "73", isCorrect: false },
+    ],
+  },
+  {
+    id: 17,
+    question: "What is 8 + 2?",
+    options: [
+      { text: "10", isCorrect: true },
+      { text: "9", isCorrect: false },
+      { text: "11", isCorrect: false },
+      { text: "82", isCorrect: false },
+    ],
+  },
+  {
+    id: 18,
+    question: "What is 6 + 3?",
+    options: [
+      { text: "9", isCorrect: true },
+      { text: "8", isCorrect: false },
+      { text: "10", isCorrect: false },
+      { text: "63", isCorrect: false },
+    ],
+  },
+  {
+    id: 19,
+    question: "What is 5 + 4?",
+    options: [
+      { text: "9", isCorrect: true },
+      { text: "8", isCorrect: false },
+      { text: "10", isCorrect: false },
+      { text: "54", isCorrect: false },
+    ],
+  },
+  {
+    id: 20,
+    question: "What is 2 + 8?",
+    options: [
+      { text: "10", isCorrect: true },
+      { text: "9", isCorrect: false },
+      { text: "11", isCorrect: false },
+      { text: "28", isCorrect: false },
+    ],
+  },
+  {
+    id: 21,
+    question: "What is 1 + 9?",
+    options: [
+      { text: "10", isCorrect: true },
+      { text: "9", isCorrect: false },
+      { text: "11", isCorrect: false },
+      { text: "19", isCorrect: false },
+    ],
+  },
+  {
+    id: 22,
+    question: "What is 3 + 6?",
+    options: [
+      { text: "9", isCorrect: true },
+      { text: "8", isCorrect: false },
+      { text: "10", isCorrect: false },
+      { text: "36", isCorrect: false },
+    ],
+  },
+  {
+    id: 23,
+    question: "What is 4 + 5?",
+    options: [
+      { text: "9", isCorrect: true },
+      { text: "8", isCorrect: false },
+      { text: "10", isCorrect: false },
+      { text: "45", isCorrect: false },
+    ],
+  },
+  {
+    id: 24,
+    question: "What is 7 + 2?",
+    options: [
+      { text: "9", isCorrect: true },
+      { text: "8", isCorrect: false },
+      { text: "10", isCorrect: false },
+      { text: "72", isCorrect: false },
+    ],
+  },
+  {
+    id: 25,
+    question: "What is 6 + 5?",
+    options: [
+      { text: "11", isCorrect: true },
+      { text: "10", isCorrect: false },
+      { text: "12", isCorrect: false },
+      { text: "65", isCorrect: false },
+    ],
+  },
+  {
+    id: 26,
+    question: "What is 8 + 3?",
+    options: [
+      { text: "11", isCorrect: true },
+      { text: "10", isCorrect: false },
+      { text: "12", isCorrect: false },
+      { text: "83", isCorrect: false },
+    ],
+  },
+  {
+    id: 27,
+    question: "What is 9 + 4?",
+    options: [
+      { text: "13", isCorrect: true },
+      { text: "12", isCorrect: false },
+      { text: "14", isCorrect: false },
+      { text: "94", isCorrect: false },
+    ],
+  },
+  {
+    id: 28,
+    question: "What is 5 + 6?",
+    options: [
+      { text: "11", isCorrect: true },
+      { text: "10", isCorrect: false },
+      { text: "12", isCorrect: false },
+      { text: "56", isCorrect: false },
+    ],
+  },
+  {
+    id: 29,
+    question: "What is 7 + 4?",
+    options: [
+      { text: "11", isCorrect: true },
+      { text: "10", isCorrect: false },
+      { text: "12", isCorrect: false },
+      { text: "74", isCorrect: false },
+    ],
+  },
+  {
+    id: 30,
+    question: "What is 6 + 7?",
+    options: [
+      { text: "13", isCorrect: true },
+      { text: "12", isCorrect: false },
+      { text: "14", isCorrect: false },
+      { text: "67", isCorrect: false },
+    ],
+  },
+  {
+    id: 31,
+    question: "What is 8 + 4?",
+    options: [
+      { text: "12", isCorrect: true },
+      { text: "11", isCorrect: false },
+      { text: "13", isCorrect: false },
+      { text: "84", isCorrect: false },
+    ],
+  },
+  {
+    id: 32,
+    question: "What is 9 + 5?",
+    options: [
+      { text: "14", isCorrect: true },
+      { text: "13", isCorrect: false },
+      { text: "15", isCorrect: false },
+      { text: "95", isCorrect: false },
+    ],
+  },
+  {
+    id: 33,
+    question: "What is 5 + 8?",
+    options: [
+      { text: "13", isCorrect: true },
+      { text: "12", isCorrect: false },
+      { text: "14", isCorrect: false },
+      { text: "58", isCorrect: false },
+    ],
+  },
+  {
+    id: 34,
+    question: "What is 6 + 9?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "69", isCorrect: false },
+    ],
+  },
+  {
+    id: 35,
+    question: "What is 7 + 5?",
+    options: [
+      { text: "12", isCorrect: true },
+      { text: "11", isCorrect: false },
+      { text: "13", isCorrect: false },
+      { text: "75", isCorrect: false },
+    ],
+  },
+  {
+    id: 36,
+    question: "What is 11 + 7?",
+    options: [
+      { text: "18", isCorrect: true },
+      { text: "17", isCorrect: false },
+      { text: "19", isCorrect: false },
+      { text: "117", isCorrect: false },
+    ],
+  },
+  {
+    id: 37,
+    question: "What is 12 + 5?",
+    options: [
+      { text: "17", isCorrect: true },
+      { text: "16", isCorrect: false },
+      { text: "18", isCorrect: false },
+      { text: "125", isCorrect: false },
+    ],
+  },
+  {
+    id: 38,
+    question: "What is 13 + 6?",
+    options: [
+      { text: "19", isCorrect: true },
+      { text: "18", isCorrect: false },
+      { text: "20", isCorrect: false },
+      { text: "136", isCorrect: false },
+    ],
+  },
+  {
+    id: 39,
+    question: "What is 14 + 8?",
+    options: [
+      { text: "22", isCorrect: true },
+      { text: "21", isCorrect: false },
+      { text: "23", isCorrect: false },
+      { text: "148", isCorrect: false },
+    ],
+  },
+  {
+    id: 40,
+    question: "What is 15 + 4?",
+    options: [
+      { text: "19", isCorrect: true },
+      { text: "18", isCorrect: false },
+      { text: "20", isCorrect: false },
+      { text: "154", isCorrect: false },
+    ],
+  },
+  {
+    id: 41,
+    question: "What is 16 + 3?",
+    options: [
+      { text: "19", isCorrect: true },
+      { text: "18", isCorrect: false },
+      { text: "20", isCorrect: false },
+      { text: "163", isCorrect: false },
+    ],
+  },
+  {
+    id: 42,
+    question: "What is 17 + 2?",
+    options: [
+      { text: "19", isCorrect: true },
+      { text: "18", isCorrect: false },
+      { text: "20", isCorrect: false },
+      { text: "172", isCorrect: false },
+    ],
+  },
+  {
+    id: 43,
+    question: "What is 18 + 1?",
+    options: [
+      { text: "19", isCorrect: true },
+      { text: "18", isCorrect: false },
+      { text: "20", isCorrect: false },
+      { text: "181", isCorrect: false },
+    ],
+  },
+  {
+    id: 44,
+    question: "What is 19 + 0?",
+    options: [
+      { text: "19", isCorrect: true },
+      { text: "18", isCorrect: false },
+      { text: "20", isCorrect: false },
+      { text: "190", isCorrect: false },
+    ],
+  },
+  {
+    id: 45,
+    question: "What is 20 + 5?",
+    options: [
+      { text: "25", isCorrect: true },
+      { text: "24", isCorrect: false },
+      { text: "26", isCorrect: false },
+      { text: "205", isCorrect: false },
+    ],
+  },
+  {
+    id: 46,
+    question: "What is 21 + 4?",
+    options: [
+      { text: "25", isCorrect: true },
+      { text: "24", isCorrect: false },
+      { text: "26", isCorrect: false },
+      { text: "214", isCorrect: false },
+    ],
+  },
+  {
+    id: 47,
+    question: "What is 22 + 3?",
+    options: [
+      { text: "25", isCorrect: true },
+      { text: "24", isCorrect: false },
+      { text: "26", isCorrect: false },
+      { text: "223", isCorrect: false },
+    ],
+  },
+  {
+    id: 48,
+    question: "What is 23 + 2?",
+    options: [
+      { text: "25", isCorrect: true },
+      { text: "24", isCorrect: false },
+      { text: "26", isCorrect: false },
+      { text: "232", isCorrect: false },
+    ],
+  },
+  {
+    id: 49,
+    question: "What is 24 + 1?",
+    options: [
+      { text: "25", isCorrect: true },
+      { text: "24", isCorrect: false },
+      { text: "26", isCorrect: false },
+      { text: "241", isCorrect: false },
+    ],
+  },
+  {
+    id: 50,
+    question: "What is 25 + 0?",
+    options: [
+      { text: "25", isCorrect: true },
+      { text: "24", isCorrect: false },
+      { text: "26", isCorrect: false },
+      { text: "250", isCorrect: false },
     ],
   },
 ];
---
+++ app/data/math/subtraction.js
@@ -0,0 +1,502 @@
+export const subtractionQuestions = [
+  {
+    id: 1,
+    question: "What is 10 - 4?",
+    options: [
+      { text: "6", isCorrect: true },
+      { text: "5", isCorrect: false },
+      { text: "4", isCorrect: false },
+      { text: "14", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "What is 9 - 3?",
+    options: [
+      { text: "6", isCorrect: true },
+      { text: "5", isCorrect: false },
+      { text: "7", isCorrect: false },
+      { text: "93", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "What is 8 - 2?",
+    options: [
+      { text: "6", isCorrect: true },
+      { text: "5", isCorrect: false },
+      { text: "4", isCorrect: false },
+      { text: "82", isCorrect: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "What is 7 - 5?",
+    options: [
+      { text: "2", isCorrect: true },
+      { text: "1", isCorrect: false },
+      { text: "3", isCorrect: false },
+      { text: "75", isCorrect: false },
+    ],
+  },
+  {
+    id: 5,
+    question: "What is 12 - 7?",
+    options: [
+      { text: "5", isCorrect: true },
+      { text: "6", isCorrect: false },
+      { text: "4", isCorrect: false },
+      { text: "127", isCorrect: false },
+    ],
+  },
+  {
+    id: 6,
+    question: "What is 15 - 8?",
+    options: [
+      { text: "7", isCorrect: true },
+      { text: "6", isCorrect: false },
+      { text: "8", isCorrect: false },
+      { text: "158", isCorrect: false },
+    ],
+  },
+  {
+    id: 7,
+    question: "What is 11 - 6?",
+    options: [
+      { text: "5", isCorrect: true },
+      { text: "4", isCorrect: false },
+      { text: "6", isCorrect: false },
+      { text: "116", isCorrect: false },
+    ],
+  },
+  {
+    id: 8,
+    question: "What is 14 - 9?",
+    options: [
+      { text: "5", isCorrect: true },
+      { text: "6", isCorrect: false },
+      { text: "4", isCorrect: false },
+      { text: "149", isCorrect: false },
+    ],
+  },
+  {
+    id: 9,
+    question: "What is 13 - 7?",
+    options: [
+      { text: "6", isCorrect: true },
+      { text: "5", isCorrect: false },
+      { text: "7", isCorrect: false },
+      { text: "137", isCorrect: false },
+    ],
+  },
+  {
+    id: 10,
+    question: "What is 16 - 8?",
+    options: [
+      { text: "8", isCorrect: true },
+      { text: "7", isCorrect: false },
+      { text: "9", isCorrect: false },
+      { text: "168", isCorrect: false },
+    ],
+  },
+  {
+    id: 11,
+    question: "What is 20 - 5?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "205", isCorrect: false },
+    ],
+  },
+  {
+    id: 12,
+    question: "What is 18 - 9?",
+    options: [
+      { text: "9", isCorrect: true },
+      { text: "8", isCorrect: false },
+      { text: "10", isCorrect: false },
+      { text: "189", isCorrect: false },
+    ],
+  },
+  {
+    id: 13,
+    question: "What is 17 - 4?",
+    options: [
+      { text: "13", isCorrect: true },
+      { text: "12", isCorrect: false },
+      { text: "14", isCorrect: false },
+      { text: "174", isCorrect: false },
+    ],
+  },
+  {
+    id: 14,
+    question: "What is 19 - 7?",
+    options: [
+      { text: "12", isCorrect: true },
+      { text: "11", isCorrect: false },
+      { text: "13", isCorrect: false },
+      { text: "197", isCorrect: false },
+    ],
+  },
+  {
+    id: 15,
+    question: "What is 21 - 6?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "216", isCorrect: false },
+    ],
+  },
+  {
+    id: 16,
+    question: "What is 22 - 9?",
+    options: [
+      { text: "13", isCorrect: true },
+      { text: "12", isCorrect: false },
+      { text: "14", isCorrect: false },
+      { text: "229", isCorrect: false },
+    ],
+  },
+  {
+    id: 17,
+    question: "What is 25 - 8?",
+    options: [
+      { text: "17", isCorrect: true },
+      { text: "16", isCorrect: false },
+      { text: "18", isCorrect: false },
+      { text: "258", isCorrect: false },
+    ],
+  },
+  {
+    id: 18,
+    question: "What is 30 - 15?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "3015", isCorrect: false },
+    ],
+  },
+  {
+    id: 19,
+    question: "What is 27 - 12?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "2712", isCorrect: false },
+    ],
+  },
+  {
+    id: 20,
+    question: "What is 24 - 9?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "249", isCorrect: false },
+    ],
+  },
+  {
+    id: 21,
+    question: "What is 26 - 11?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "2611", isCorrect: false },
+    ],
+  },
+  {
+    id: 22,
+    question: "What is 23 - 8?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "238", isCorrect: false },
+    ],
+  },
+  {
+    id: 23,
+    question: "What is 28 - 13?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "2813", isCorrect: false },
+    ],
+  },
+  {
+    id: 24,
+    question: "What is 32 - 17?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "3217", isCorrect: false },
+    ],
+  },
+  {
+    id: 25,
+    question: "What is 35 - 20?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "3520", isCorrect: false },
+    ],
+  },
+  {
+    id: 26,
+    question: "What is 40 - 25?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "4025", isCorrect: false },
+    ],
+  },
+  {
+    id: 27,
+    question: "What is 45 - 30?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "4530", isCorrect: false },
+    ],
+  },
+  {
+    id: 28,
+    question: "What is 50 - 35?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "5035", isCorrect: false },
+    ],
+  },
+  {
+    id: 29,
+    question: "What is 55 - 40?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "5540", isCorrect: false },
+    ],
+  },
+  {
+    id: 30,
+    question: "What is 60 - 45?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "6045", isCorrect: false },
+    ],
+  },
+  {
+    id: 31,
+    question: "What is 65 - 50?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "6550", isCorrect: false },
+    ],
+  },
+  {
+    id: 32,
+    question: "What is 70 - 55?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "7055", isCorrect: false },
+    ],
+  },
+  {
+    id: 33,
+    question: "What is 75 - 60?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "7560", isCorrect: false },
+    ],
+  },
+  {
+    id: 34,
+    question: "What is 80 - 65?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "8065", isCorrect: false },
+    ],
+  },
+  {
+    id: 35,
+    question: "What is 85 - 70?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "8570", isCorrect: false },
+    ],
+  },
+  {
+    id: 36,
+    question: "What is 90 - 75?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "9075", isCorrect: false },
+    ],
+  },
+  {
+    id: 37,
+    question: "What is 95 - 80?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "9580", isCorrect: false },
+    ],
+  },
+  {
+    id: 38,
+    question: "What is 100 - 85?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "10085", isCorrect: false },
+    ],
+  },
+  {
+    id: 39,
+    question: "What is 105 - 90?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "10590", isCorrect: false },
+    ],
+  },
+  {
+    id: 40,
+    question: "What is 110 - 95?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "11095", isCorrect: false },
+    ],
+  },
+  {
+    id: 41,
+    question: "What is 115 - 100?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "115100", isCorrect: false },
+    ],
+  },
+  {
+    id: 42,
+    question: "What is 120 - 105?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "120105", isCorrect: false },
+    ],
+  },
+  {
+    id: 43,
+    question: "What is 125 - 110?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "125110", isCorrect: false },
+    ],
+  },
+  {
+    id: 44,
+    question: "What is 130 - 115?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "130115", isCorrect: false },
+    ],
+  },
+  {
+    id: 45,
+    question: "What is 135 - 120?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "135120", isCorrect: false },
+    ],
+  },
+  {
+    id: 46,
+    question: "What is 140 - 125?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "140125", isCorrect: false },
+    ],
+  },
+  {
+    id: 47,
+    question: "What is 145 - 130?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "145130", isCorrect: false },
+    ],
+  },
+  {
+    id: 48,
+    question: "What is 150 - 135?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "150135", isCorrect: false },
+    ],
+  },
+  {
+    id: 49,
+    question: "What is 155 - 140?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "155140", isCorrect: false },
+    ],
+  },
+  {
+    id: 50,
+    question: "What is 160 - 145?",
+    options: [
+      { text: "15", isCorrect: true },
+      { text: "14", isCorrect: false },
+      { text: "16", isCorrect: false },
+      { text: "160145", isCorrect: false },
+    ],
+  },
+];
